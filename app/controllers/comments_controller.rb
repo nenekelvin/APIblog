6cc6b4f3e7bea62d3ac294580fee53ef0f3ee6f5
nenekelvin <karvinmitchar@gmail.com>
2019-09-06 14:19:02 +0300

    fix error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,9 @@ class CommentsController < ApplicationController
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
- 
+
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:commentor, :body)
     end
 end
